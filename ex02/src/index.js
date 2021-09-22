b0ac6f9e00d4cc418f95101e0dbd450316923c01
Askolini <asmircc@gmail.com>
2021-09-22 11:33:39 +0200

    If

--- ex02/src/index.js
+++ ex02/src/index.js
@@ -1,10 +1,11 @@
 function myFunction(myParameter) {
     if(myParametar) {
+        myFunction(true);
         return "The parameter is true!";
     }
+    myFunction(false);
     return "The parameter is false!";
 }
-myFunction(true);
-myFunction(false);
+
 
 module.exports = myFunction;
