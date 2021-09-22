c9ffba0426bed3d4d49b42998eebc40147be2231
Askolini <asmircc@gmail.com>
2021-09-22 11:18:40 +0200

    If Statements

---
+++ ex02/src/index.js
@@ -0,0 +1,10 @@
+function myFunction(myParameter) {
+    if(myParametar) {
+        return "The parameter is true!";
+    }
+    return "The parameter is false!";
+}
+myFunction(true);
+myFunction(false);
+
+module.exports = myFunction;
